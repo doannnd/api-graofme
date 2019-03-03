15d80ca8841a5f0842fae2eccd772ea47d52f5f7
doannd <nguyendinhdoanjdk@gmail.com>
2019-03-03 16:46:57 +0700

    Building the user route

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const chalk = require('chalk');
 
 require('dotenv').config();
 
+const userRoute = require(__dirname + '/api/routes/user.route');
+
 const app = express();
 const port = process.env.PORT;
+
+app.use('/user', userRoute);
 
 app.listen(port, () => {
     console.log(chalk.blue('Server is running on port ', port));
